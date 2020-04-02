8e639d4fa08138f62fbe11a92025bece6f647195
AtrixMU <matas@udris.lt>
2020-04-02 17:13:43 +0300

    Fix borrow checker errors

--- project/src/main.rs
+++ project/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let test_vm = VMProcessor::new();
     println!("{:?}", test_vm);
     let test = RMProcessor::new();
-    println!("{:?}", test);
+    // println!("{:?}", test);
     println!("{:?}", test.get_zero_flag());
     println!("{:?}", test_vm.get_supervisor_flag());
     println!("{:?}", 0 as char);
