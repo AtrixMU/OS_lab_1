242c68ea9c3dec18790be3b640209446b0c84cfc
Robertas Povedionok <ROBKE223@gmail.com>
2020-05-27 00:17:16 +0300

    Added Resource_list

--- project/src/operating_system/mod.rs
+++ project/src/operating_system/mod.rs
@@ -1,3 +1,4 @@
 pub mod kernel;
 pub mod process;
 pub mod resource;
+pub mod resource_list;
---
+++ project/src/operating_system/resource_list.rs
@@ -0,0 +1,52 @@
+use crate::consts::*;
+use super::resource::Resource;
+
+#[derive(Debug)]
+pub struct ResourceList {
+    free: Vec<Resource>
+}
+
+impl ResourceList{
+    pub fn new() -> ResourceList {
+        ResourceList{
+            free: Vec::new(),
+        }
+    }
+
+    pub fn add(&mut self, resource: Resource) {
+        self.free.push(resource);
+
+    }
+
+    pub fn take(&mut self, resource_type: usize ) ->  Option<Resource> {
+        let mut res_index = 0;
+        let mut found = false;
+        for (index, res) in self.free.iter().enumerate() {
+            if res.get_type() == resource_type && res.get_recipient().is_none() {
+              res_index = index;
+              found = true;
+              break;
+            }
+        }
+        if !found {
+            return None;
+        }
+        Some(self.free.remove(res_index))
+    }
+
+    pub fn take_specific(&mut self, resource_type: usize, recipient: usize ) -> Option<Resource> {
+        let mut res_index = 0;
+        let mut found = false;
+        for (index, res) in self.free.iter().enumerate() {
+            if res.get_type() == resource_type && res.get_recipient() == Some(recipient) {
+                res_index = index;
+                found = true;
+                break;
+            }
+        }
+        if !found {
+            return None;
+        }
+        Some(self.free.remove(res_index))
+    }
+}
