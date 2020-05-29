454c0abee100b65c7782c5c6fc9e0698b1d4475a
Robertas Povedionok <ROBKE223@gmail.com>
2020-05-29 04:41:17 +0300

    Making a planner

--- project/src/operating_system/kernel.rs
+++ project/src/operating_system/kernel.rs
@@ -1,5 +1,8 @@
-use super::process::Process;
+//use super::process::Process;
 use super::resource_list::ResourceList;
+use super::process::Process;
+use crate::consts::*;
+use crate::real_machine::processor::RMProcessor;
 
 pub struct Kernel {
     process_list: Vec<Box<dyn Process>>,
@@ -14,4 +17,28 @@ impl Kernel {
             resources: ResourceList::new(),
         }
     }
+}
+
+impl Kernel {
+    pub fn planner( &mut self, rm:&mut RMProcessor) {
+        self.process_list.sort_by_key(|d| d.get_priority());
+        let i = 0;
+        
+        while true  {
+            for process in &mut self.process_list {
+                if process.get_state() == P_BLOCKED {
+                    let result =    process.step(rm);
+                    if result.0.is_some() && result.3.is_none() {
+                        let value = result.0.unwrap();
+
+                    }
+                }
+                
+            
+
+            }
+
+        }
+
+    } 
 }
